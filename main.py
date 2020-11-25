649031ba6aea1530a34165f8cd0fb0f050ddc068
ayfokin <ayfokin@mail.ru>
2020-11-25 20:45:06 +0300

    added fibonacci method

--- main.py
+++ main.py
@@ -45,6 +45,7 @@ def dichotomy(function, epsilon):
             right_border = x2
         if right_border - left_border < epsilon:
             break
+    return x1
 
 
 def golden_ratio(function, epsilon):
@@ -66,14 +67,30 @@ def golden_ratio(function, epsilon):
             right_border = x2
         if right_border - left_border < epsilon:
             break
+    return x1
 
 
 def fibonacci(function, epsilon):
     n = get_n(function[1], function[2], epsilon)
-    x1 = function[1]
-    for i in range(n):
-
-    return 1
+    a = function[1]
+    b = function[2]
+    x1 = a + get_fibonacci(n) / get_fibonacci(n + 2) * (b - a)
+    x2 = a + get_fibonacci(n + 1) / get_fibonacci(n + 2) * (b - a)
+    for k in range(n):
+        k += 1
+        fx1 = function[0](x1)
+        fx2 = function[0](x2)
+        if fx1 < fx2:
+            b = x2
+            x2 = x1
+            x1 = a + get_fibonacci(n - k + 1) / get_fibonacci(n - k + 3) * (b - a)
+        if fx1 > fx2:
+            a = x1
+            x1 = x2
+            x2 = a + get_fibonacci(n - k + 2) / get_fibonacci(n - k + 3) * (b - a)
+        if fx1 == fx2:
+            return x1
+    return x1
 
 
 def get_fibonacci(n):
@@ -86,9 +103,3 @@ def get_n(left_border, right_border, epsilon):
 
 
 functions = [[f1, -0.5, 0.5], [f2, 6, 9.9], [f3, 0, 2 * math.pi], [f4, 0, 1], [f5, 0.5, 2.5]]
-print(get_n(-0.5, 0.5, 0.00001))
-
-#for function in functions:
- #   iteration = 0
-  #  for epsilon in range(NUMBER_OF_EPSILON):
-   #     epsilon = epsilon * 1 / NUMBER_OF_EPSILON + 1 / NUMBER_OF_EPSILON
